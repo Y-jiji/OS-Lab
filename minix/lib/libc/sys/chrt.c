cef318a3c847e3057a7752d28d1d5c8616fa0ef1
Y-jiji <758725271@qq.com>
2022-04-19 21:27:16 +0800

    handle chrt(0)

--- minix/lib/libc/sys/chrt.c
+++ minix/lib/libc/sys/chrt.c
@@ -10,6 +10,9 @@ int chrt(unsigned long ddl){
     alarm(ddl);
     message m;
     memset(&m, 0, sizeof(m));
-    m.m_u32.data[0] = ddl + (unsigned)(time(NULL));
+    if (ddl != 0)
+        m.m_u32.data[0] = ddl + (unsigned)(time(NULL));
+    else
+        m.m_u32.data[0] = ~0u;
     return(_syscall(PM_PROC_NR, PM_CHRT, &m));
 }
