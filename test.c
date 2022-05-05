b0de92057c4b7941cace32778a1e73bd8a722445
Y-jiji <758725271@qq.com>
2022-05-05 11:52:59 +0800

    Fix: add read/write fail debug output

--- test.c
+++ test.c
@@ -99,7 +99,7 @@ void do_test(
 
     printf(
         "test [%04d] proc [%04d] alarm rings\n"
-        "byte cnt [%04ld]\n",
+        "byte cnt [%04lld]\n",
         test_id, proc_id, byte_cnt
     );
     /* calculate throughput, print data */
